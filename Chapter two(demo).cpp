6c9818a70ef7e81f56b1f7675534073be3637bce
Bekaluminbale <129955566+Bekaluminbale@users.noreply.github.com>
2023-04-06 13:47:49 +0300

    Update and rename Chapter two(demo).txt to Chapter two(demo).cpp

--- Chapter two(demo).cpp
+++ Chapter two(demo).cpp
@@ -1,5 +1,4 @@
-﻿//Chapter 2 (demo)
-//preprocessor directives
+﻿//preprocessor directives
 /*preprocessor directives are instructions to the compiler that are executed before the actual compilation process, and they are identified by a # symbol at the beginning of the line.*/
 //example
 #include <iostream>
@@ -9,8 +8,7 @@ int main() {
     double radius = 5.0;
     double area = PI * radius * radius;
     cout << "The area of a circle with radius " << radius << " is " << area << endl;
-    return 0;
-}
+    
 
 
 
@@ -21,17 +19,14 @@ int main() {
 //C++ identifiers
 /* identifier is a name used to identify a variable, function, class, module, or any other user-defined item.*/
 //Example
-#include <iostream>
-using namespace std;
-int main() {
+
     int age = 25;
     float height = 1.75;
     string name = "John";
     cout << "Name: " << name << endl;
     cout << "Age: " << age << endl;
     cout << "Height: " << height << endl;
-    return 0;
-}
+    
 
 //C++ Comments
 //used to explain C++ code, and to make it more readable.
@@ -57,21 +52,15 @@ cout << x + y + z;
 //assign a variable 
 /*sets the value of the memory location associated with the variable to the specified value*/
 //example 
-#include <iostream>
-using namespace std;
-int main() {
+
   int num = 5; // assigning the value 5 to the variable num
   cout << "The value of num is: " << num << endl;
-  return 0;
-}
-
+  
 
 //basic data types 
 //variable in C++ must be a specified data type:
 //example
-#include <iostream>
-using namespace std;
-int main() {
+
   int my_int = 42; // integer (whole number) type
   double my_double = 3.14; // floating-point (decimal) type
   char my_char = 'a'; // character type
@@ -80,8 +69,7 @@ int main() {
   cout << my_double << endl;
   cout << my_char << endl;
   cout << my_bool << endl;
-  return 0;
-}
+  
 
 //characters
 //The char data type is used to store a single character
@@ -91,41 +79,32 @@ cout << myGrade;
 
 
 //constants 
-*/are used to define values that cannot be changed during the execution of a program/*
-#include <iostream>
-using namespace std;
-int main() {
+/*are used to define values that cannot be changed during the execution of a program*/
+
     const int MAX_VALUE = 50;
-    const float PI = 3.14;
     const string GREETING = "Hello, World!";
     cout << "The maximum value is: " << MAX_VALUE << endl;
     cout << "The value of pi is: " << PI << endl;
     cout << GREETING << endl;
-    return 0;
-}
+    
 
 //input /output statements 
 /*refer to the process of sending data into a program (input) and receiving data from a program (output), respectively.*/
 //example
-#include <iostream>
-using namespace std;
-int main() {
-   int age;
-   string name;
+
+   
+  
    cout << "Enter your name: ";
    cin >> name; 
    cout << "Enter your age: ";
    cin >> age; 
    cout << "Hello, " << name << "! You are " << age << " years old." << endl;   
-   return 0;
-}
+   
 
 //operators
 //arthimetic operators
 //example
-#include <iostream>
-using namespace std;
-int main() {
+
     int num1 = 10;
     int num2 = 5;
     // addition
@@ -143,18 +122,15 @@ int main() {
     // modulus (remainder)
     int remainder = num1 % num2;
     cout << "Remainder: " << remainder << endl;   
-    return 0;
-}
+    
 //assignment operators
 //used to assign values to variables.
-int x = 10;
+int t = 10;
 
 //relational operators 
 /*Relational operators in C++ are used to compare two values and return a boolean value (true or false) based on the result of the comparison.*/
 //example
-#include <iostream>
-using namespace std;
-int main() {
+
     int a = 10;
     int b = 5;
     // greater than operator
@@ -181,94 +157,75 @@ int main() {
     if (a != b) {
         cout << "a is not equal to b" << endl;
     }
-    return 0;
-}
+    
 
 
 //logical operators 
 //are used to determine the logic between variables or values:
 //example
-#include <iostream>
-using namespace std;
-int main() {
-   int x = 10;
-   int y = 20;
-   if (x == 10 && y == 20) {
-      cout << "x is equal to 10 and y is equal to 20" << endl;
+
+   int h= 10;
+   int i= 20;
+   if (h== 10 && i == 20) {
+      cout << "h is equal to 10 and i is equal to 20" << endl;
    }  
-   if (x == 10 || y == 30) {
-      cout << "Either x is equal to 10 or y is equal to 30" << endl;
+   if (h == 10 || i == 30) {
+      cout << "Either h is equal to 10 or i is equal to 30" << endl;
    }  
-   if (!(x == 5)) {
-      cout << "x is not equal to 5" << endl;
+   if (!(h == 5)) {
+      cout << "h is not equal to 5" << endl;
    }
-   return 0;
-}
+   
 
 
 //bitwise operators 
 //Bitwise operators in C++ are used to manipulate individual bits within a value.
 //example
-#include <iostream>
-using namespace std;
-int main() {
-    int a = 5;     // 0101 in binary
-    int b = 3;     // 0011 in binary
-    int c = a & b; // 0001 in binary (result of bitwise AND)
+
+    int n = 5;     // 0101 in binary
+    int r = 3;     // 0011 in binary
+    int c = n & r; // 0001 in binary (result of bitwise AND)
     cout <<"c is="<<c<<endl;   // prints 1
-    return 0;
-}
+    
 
 
 
 //increment/decrement operators 
-#include <iostream>
-using namespace std;
-int main() {
-    int a = 5;
-    int b = a++; // b = 5, a = 6
-    int c = ++a; // c = 7, a = 7
+
+    int s = 5;
+    int o = s++; // o = 5, s = 6
+    int u = ++s; // u = 7, s = 7
     int d = 10;
     int e = --d; // e = 9, d = 9
     int f = d--; // f = 9, d = 8
-    cout << b << " " << c << " " << e << " " << f;
-    return 0;
-}
+    cout << o << " " << c << " " << e << " " << f;
+    
 
 //precedence  of operators
 //are used to determine the order in which operations are executed in an expression.
 //example
-#include <iostream>
-using namespace std;
-int main() {
-    int x = 5, y = 10, z = 15;
-    int result1 = x + y * z; // Multiply y and z first, then add x
-    int result2 = (x + y) * z; // Add x and y first, then multiply by z
+
+    int m = 5, l = 10, q= 15;
+    int result1 = m + l* q; // Multiply l and q first, then add m
+    int result2 = (m + l) * q; // Add m and l first, then multiply by q
     cout << "Result 1: " << result1 << endl; // Output: 155
     cout << "Result 2: " << result2 << endl; // Output: 225
-    return 0;
-}
+    
 
 //simple type convesion
 //the process of converting a value of one data type to another data type.
 
 //example
 
-#include <iostream>
-using namespace std;
-int main() {
+
     // Implicit conversion
-    int x = 5;
-    float y = x;
-    cout << "Implicit conversion: " << x << " (int) to " << y << " (float)" << endl;
+    int p = 5;
+    float v = p;
+    cout << "Implicit conversion: " << x << " (int) to " << v << " (float)" << endl;
     
     // Explicit conversion
-    float a = 3.14;
-    int b = (int) a;
-    cout << "Explicit conversion: " << a << " (float) to " << b << " (int)" << endl;   
+    float w = 3.14;
+    int g = (int) w;
+    cout << "Explicit conversion: " << a << " (float) to " << g << " (int)" << endl;   
     return 0;
 }
-
-
-
-
